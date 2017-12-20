2a39006e429fde8afe653d4242fd8acefc91a48d
Charles Osoti <charlesosoti@gmail.com>
2017-12-20 12:31:15 +0300

    Added User Registration

--- account/urls.py
+++ account/urls.py
@@ -3,6 +3,7 @@ from django.contrib.auth import views as auth_views
 from . import views 
 
 urlpatterns = [
+    url(r'^register/$', views.register, name='register'),
 	url(r'^login/$', auth_views.LoginView.as_view(), name='login'),
     url(r'^logout/$', auth_views.LogoutView.as_view(), name='logout'),
     url(r'^logout-then-login/$', auth_views.logout_then_login, name='logout_then_login'),
